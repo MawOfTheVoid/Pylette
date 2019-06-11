d81b24b8e61d178aaa45e5e8c6be30f6fd079912
The Void <MawOfTheVoid@protonmail.com>
2019-06-11 21:27:17 +0200

    Added more supported Picture formats.

--- filemanager.py
+++ filemanager.py
@@ -3,7 +3,7 @@ import pictureobject
 
 
 class Filemanager():
-    """Provides an interface for dataobjects and creates them
+    """Provides an interface for dataobjects or creates them
     on demand"""
 
     def __init__(self):
@@ -34,8 +34,9 @@ class Filemanager():
         # update the gui
 
         # TODO add more filextensions
-        if (filepath[-4:] == ".png"):
+        if self.is_picture(filepath):
             picture = pictureobject.Picture_object(filepath)
+            print(picture.filename)
             self.picture_objects.append(picture)
         pass
 
@@ -57,3 +58,20 @@ class Filemanager():
         # add the new colors to the list
         # return
         pass
+
+    def is_picture(self, filepath):
+        if (
+            filepath[-4:] == ".png" or filepath[-4:] == ".gif" or
+            filepath[-4:] == ".bmp" or filepath[-4:] == ".eps" or
+            filepath[-4:] == ".dib" or filepath[-4:] == ".ico" or
+            filepath[-4:] == ".msp" or filepath[-4:] == ".pcx" or
+            filepath[-4:] == ".ppm" or filepath[-4:] == ".sgi" or
+            filepath[-4:] == ".tga" or filepath[-4:] == ".jpg"
+                ):
+            return True
+        elif filepath[-5:] == ".jpeg" or filepath[-5:] == ".tiff":
+            return True
+        elif filepath[-3:] == ".im":
+            return True
+        else:
+            return False

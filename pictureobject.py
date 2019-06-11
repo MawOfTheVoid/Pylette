1d3eb294e855b5899c7fbf703b40b748e4306cbc
The Void <MawOfTheVoid@protonmail.com>
2019-06-11 22:39:48 +0200

    A pictureobject now can get its colors from a file. Gifs dont work though.

--- pictureobject.py
+++ pictureobject.py
@@ -1,4 +1,6 @@
 from PIL import Image
+from PyQt5.QtGui import QColor
+import gc
 
 
 class Picture_object():
@@ -17,13 +19,31 @@ class Picture_object():
         return filepath[-1]
 
     def get_list_from_file(self, filepath):
-        # öffnet datei mit pillow
+        # öffnet datei mit pillow  	✓
         # checkt ob die länge der zweiten unterliste4 lang ist
         # wenn nicht dann 255 anhängen
         # in QColors umwandeln
         # liste in tulpe umwandeln
         # liste zurückgeben
-        pass
+
+        gc.collect()
+        img = Image.open(filepath)
+        colorlist = img.getcolors(maxcolors=3840 * 2160)
+        qcolorlist = []
+        """TODO Gifs do not Work!!!"""
+        # picture has no alpha channel
+        if len(colorlist[0][1]) == 3:
+            for count_color in colorlist:
+                r, g, b = count_color[1]
+                qcolorlist.append((count_color[0], QColor.fromRgb(r, g, b)))
+        # picture has alpha channel
+        elif len(colorlist[0][1]) == 4:
+            for count_color in colorlist:
+                r, g, b, a = count_color[1]
+                qcolorlist.append(
+                    (count_color[0], QColor.fromRgb(r, g, b, alpha=a))
+                )
+        return qcolorlist
 
     def reset_list(self):
         # mutable_list = []

420db4e18cbf7138101c40349b08d70ce5ff0f3d
The Void <MawOfTheVoid@protonmail.com>
2019-06-18 21:50:19 +0200

    Outsources methods to functions in the pictureobject_functions.py file to test them more easily in the future.

--- pictureobject.py
+++ pictureobject.py
@@ -1,6 +1,4 @@
-from PIL import Image
-from PyQt5.QtGui import QColor
-import gc
+import pictureobject_functions as pic_functions
 
 
 class Picture_object():
@@ -9,37 +7,9 @@ class Picture_object():
     def __init__(self, filepath, conf):
         self.conf = conf
         self.is_changed = False
-        self.filename = self.get_filename_from_path(filepath)
-        self.unmutable_list = self.get_list_from_file(filepath)
+        self.filename = pic_functions.get_filename_from_path(filepath)
+        self.unmutable_list = pic_functions.get_list_from_file(filepath)
         self.mutable_list = self.reset_list()
-
-    def get_filename_from_path(self, filepath):
-        filepath = filepath.replace("\\", "/").split("/")
-        return filepath[-1]
-
-    def get_list_from_file(self, filepath):
-        gc.collect()
-        img = Image.open(filepath)
-        colorlist = img.getcolors(maxcolors=1000000)
-        qcolorlist = []
-        """TODO Gifs do not work!!!"""
-        # picture has no alpha channel
-        if len(colorlist[0][1]) == 3:
-            for count_color in colorlist:
-                r, g, b = count_color[1]
-                qcolorlist.append((count_color[0], QColor.fromRgb(r, g, b)))
-        # picture has alpha channel
-        elif len(colorlist[0][1]) == 4:
-            for count_color in colorlist:
-                r, g, b, a = count_color[1]
-                qcolorlist.append(
-                    (count_color[0], QColor.fromRgb(r, g, b, alpha=a))
-                )
-        print("done")
-        qcolorlist = tuple(qcolorlist)
-        del colorlist
-        gc.collect()
-        return qcolorlist
 
     def reset_list(self):
         # mutable_list = []
---
+++ pictureobject_functions.py
@@ -0,0 +1,32 @@
+from PIL import Image
+from PyQt5.QtGui import QColor
+import gc
+
+
+def get_filename_from_path(filepath):
+    filepath = filepath.replace("\\", "/").split("/")
+    return filepath[-1]
+
+
+def get_list_from_file(filepath):
+    gc.collect()
+    img = Image.open(filepath)
+    colorlist = img.getcolors(maxcolors=1000000)
+    qcolorlist = []
+    """TODO Gifs do not work!!!"""
+    # picture has no alpha channel
+    if len(colorlist[0][1]) == 3:
+        for count_color in colorlist:
+            r, g, b = count_color[1]
+            qcolorlist.append((count_color[0], QColor.fromRgb(r, g, b)))
+    # picture has alpha channel
+    elif len(colorlist[0][1]) == 4:
+        for count_color in colorlist:
+            r, g, b, a = count_color[1]
+            qcolorlist.append(
+                (count_color[0], QColor.fromRgb(r, g, b, alpha=a))
+            )
+    qcolorlist = tuple(qcolorlist)
+    del colorlist
+    gc.collect()
+    return qcolorlist
